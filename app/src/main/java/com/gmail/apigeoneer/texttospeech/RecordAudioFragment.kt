5caf4a079ae9a10c018940a0f4dec3b9f1d127cb
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-05-22 00:03:49 +0530

    Created navigation & added RecordAudioFragment to it

--- app/src/main/java/com/gmail/apigeoneer/texttospeech/RecordAudioFragment.kt
+++ app/src/main/java/com/gmail/apigeoneer/texttospeech/RecordAudioFragment.kt
@@ -5,15 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.gmail.apigeoneer.texttospeech.databinding.FragmentRecordAudioBinding
 
 class RecordAudioFragment : Fragment() {
+
+    // data binding
+    private lateinit var binding: FragmentRecordAudioBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record_audio, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_record_audio, container, false)
+        return binding.root
     }
 
 }
